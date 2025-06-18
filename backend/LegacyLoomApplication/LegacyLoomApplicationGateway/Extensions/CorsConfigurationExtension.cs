a7429736d0616243a483c852cced56ed5be61132
Rishav <140265067+IamRishavDas@users.noreply.github.com>
2025-06-18 23:59:12 +0530

    Update API endpoints and add CORS configuration
    - Removed "POST" method from `/gateway/api/users/{everything}` in `globalConfig.json` and `userService.json`.
    - Added new `/api/users` endpoint with its own configuration.
    - Introduced `CorsConfigurationExtension` class for CORS policy setup.
    - Updated `Program.cs` to apply CORS policy in service configuration and middleware.
    - Modified `appsettings.Development.json` to include CORS settings.

---
+++ backend/LegacyLoomApplication/LegacyLoomApplicationGateway/Extensions/CorsConfigurationExtension.cs
@@ -0,0 +1,21 @@
+﻿namespace LegacyLoomApplicationGateway.Extensions
+{
+    public static class CorsConfigurationExtension
+    {
+        public static void AddCorsPolicy(this IServiceCollection services, IConfiguration configuration)
+        {
+            var policyName = configuration["Cors:Policy"] ?? throw new ArgumentNullException("Cors:Policy not found!");
+            var origins = configuration["Cors:Domains"] ?? throw new ArgumentNullException("Cors:Domains not found!");
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: policyName,
+                                  policy =>
+                                  {
+                                      policy.WithOrigins(origins)
+                                            .AllowAnyHeader()
+                                            .AllowAnyMethod();
+                                  });
+            });
+        }
+    }
+}
